5fba7927162faef850bd1ec057a305f459ded477
Teletobbies <giovani.784@hotmail.com>
2025-01-28 10:32:49 +0100

    Code for Rebase, the only message in the log should be this one and the second

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Test-Web
 Dépot de test pour les cours
 first commit and that should show as "Readme modified" 
-second modification to the file should read "Second modification to the file read me"
---
+++ Test-code-for-rebase.c
@@ -0,0 +1,14 @@
+// Print numbers from 1 to 10
+#include <stdio.h>
+
+int main() {
+  int i = 0;
+
+  for (i = 1; i < 11; ++i)
+  {
+    printf("%d ", i);
+    printf("haha");
+    printf("hello");
+  }
+  return 0;
+
